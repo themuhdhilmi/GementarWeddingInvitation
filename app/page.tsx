80c93891df60f3ac7099b1400779484d0bca7d9e
Hilmi <hilmi@gementar.com>
2024-05-29 21:04:57 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import BackPage from "./components/BackPage";
 import BottomPanel from "./components/BottomPanel";
 import MainPage from "./components/MainPage";
@@ -6,7 +7,9 @@ import MainPage from "./components/MainPage";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between bg-[#baa49e]">
+      <Suspense>
       <MainPage />
+      </Suspense>
       <BackPage />
       {/* <BottomPanel/> */}
     </main>
